b512db359c96f236255352545f06c5a265c8ba66
isi <2210100@isi-montreal.com>
2023-03-02 14:39:29 -0500

    Book entity

---
+++ spjpa-intro/src/main/java/com/arsene/spjpaintro/SpjpaIntroApplication.java
@@ -0,0 +1,13 @@
+package com.arsene.spjpaintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpjpaIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpjpaIntroApplication.class, args);
+	}
+
+}
---
+++ spjpa-intro/src/test/java/com/arsene/spjpaintro/SpjpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsene.spjpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpjpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
